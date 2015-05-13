a1c20b7e72d684814a3c38fb62e1865572e9d989
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-05-13 10:38:20 +0200

    Implement main method

--- Alexander-Knueppel-Task2/src/application/App.java
+++ Alexander-Knueppel-Task2/src/application/App.java
@@ -1,10 +1,19 @@
 package application;
 
 public class App {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Configuration config = new Configuration(true);
+		try {
+			config.parseArguments(args);
+			if(config.cli()) {
+				// add cli handler
+			} else {
+				// add gui handler
+			}
+		} catch(IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+		}
 	}
 
 }
