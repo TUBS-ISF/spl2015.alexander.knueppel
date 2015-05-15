dbb19157286f69527167ea04d634c30e4e939d2e
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-05-15 16:06:12 +0200

    Use configuration file to start app

--- Alexander-Knueppel-Task2/src/application/App.java
+++ Alexander-Knueppel-Task2/src/application/App.java
@@ -1,18 +1,30 @@
 package application;
 
+import java.io.IOException;
+import java.util.logging.Level;
+
 public class App {
 	
 	public static void main(String[] args) {
 		Configuration config = new Configuration(true);
 		try {
 			config.parseArguments(args);
+			Configuration.logger.log(Level.INFO, "Arguments parsed successfully!");
 			if(config.cli()) {
+				Configuration.logger.log(Level.INFO, "Starting command line interface...");
 				// add cli handler
 			} else {
+				Configuration.logger.log(Level.INFO, "Starting graphical userinterface...");
 				// add gui handler
 			}
+			// add features
+			
 		} catch(IllegalArgumentException e) {
-			System.err.println(e.getMessage());
+			Configuration.logger.log(Level.SEVERE, e.getMessage());
+		} catch(IOException e) {
+			Configuration.logger.log(Level.SEVERE, e.getMessage());
+		} catch(Exception e) {
+			Configuration.logger.log(Level.SEVERE, e.getMessage());
 		}
 	}
 
