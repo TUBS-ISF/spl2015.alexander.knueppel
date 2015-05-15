32ec75d6880ec0062046b5774e6239ebd52ea208
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-05-15 16:07:54 +0200

    Add generic input-output-handler

---
+++ Alexander-Knueppel-Task2/src/application/iohandler/Handler.java
@@ -0,0 +1,15 @@
+package application.iohandler;
+
+import application.Configuration;
+
+public abstract class Handler {
+	protected Configuration config;
+	
+	public Handler(Configuration config) {
+		//only primitves and collections; shallow copy should be enough
+		this.config = config;
+	}
+	public abstract void run();
+	
+	
+}
