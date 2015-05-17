a13a5b18161dfbac6e7377a22e1a7c32f94144c4
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-05-17 18:34:54 +0200

    Add simple (mean, prior prob) classifier

---
+++ Alexander-Knueppel-Task2/src/framework/classifier/ZeroRClassifier.java
@@ -0,0 +1,17 @@
+package framework.classifier;
+
+import framework.DataSet;
+
+public class ZeroRClassifier implements Classifier {
+
+	public CategoricalResults classify(DataPoint data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void train(DataSet dataSet) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
