303bfa08455ac9fc008163e3ada362e1dbbc1d8f
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-05-17 14:17:21 +0200

    Optimize view handling and debugging

--- Alexander-Knueppel-Task2/src/application/App.java
+++ Alexander-Knueppel-Task2/src/application/App.java
@@ -3,34 +3,46 @@ package application;
 import java.io.IOException;
 import java.util.logging.Level;
 
+import application.iohandler.Controller;
+import application.iohandler.Handler;
 import application.iohandler.cli.CLIHandler;
 import application.iohandler.gui.GUIHandler;
 
 public class App {
 	
 	public static void main(String[] args) {
-		Configuration config = new Configuration(true);
+		Configuration config = new Configuration(true, 0, 1, 0); // development = true
 		try {
 			config.parseArguments(args);
 			Configuration.logger.log(Level.INFO, "Arguments parsed successfully!");
+			Controller.debug = config.dbg(); //looks so bad...
+			
+			Handler viewHandler;
+			
 			if(config.cli()) {
 				Configuration.logger.log(Level.INFO, "Starting command line interface...");
 				// add cli handler
-				CLIHandler cli = new CLIHandler(config);
-				cli.run();
+				viewHandler = new CLIHandler(config);
 			} else {
-				Configuration.logger.log(Level.INFO, "Starting graphical userinterface...");
+				Configuration.logger.log(Level.INFO, "Starting graphical user interface...");
 				// add gui handler
-				GUIHandler gui = new GUIHandler(config);
-				gui.run();
+				viewHandler = new GUIHandler(config);
 			}
+			//start the engine
+			viewHandler.go();
 			
 		} catch(IllegalArgumentException e) {
 			Configuration.logger.log(Level.SEVERE, e.getMessage());
+			if(config.dbg())
+				e.printStackTrace();
 		} catch(IOException e) {
 			Configuration.logger.log(Level.SEVERE, e.getMessage());
+			if(config.dbg())
+				e.printStackTrace();
 		} catch(Exception e) {
 			Configuration.logger.log(Level.SEVERE, e.getMessage());
+			if(config.dbg())
+				e.printStackTrace();
 		}
 	}
 
