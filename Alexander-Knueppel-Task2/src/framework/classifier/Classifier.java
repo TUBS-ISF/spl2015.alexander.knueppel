fefc82ff729459e7337a4509b3af07fe2ad73f3a
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-05-17 20:41:24 +0200

    Add UntrainedModelException

--- Alexander-Knueppel-Task2/src/framework/classifier/Classifier.java
+++ Alexander-Knueppel-Task2/src/framework/classifier/Classifier.java
@@ -1,8 +1,9 @@
 package framework.classifier;
 
 import framework.DataSet;
+import framework.exceptions.UntrainedModelException;
 
 public interface Classifier extends Cloneable {
-	public CategoricalResults classify(DataPoint data);
+	public CategoricalResults classify(DataPoint data) throws UntrainedModelException;
 	public void train(DataSet dataSet);
 }
