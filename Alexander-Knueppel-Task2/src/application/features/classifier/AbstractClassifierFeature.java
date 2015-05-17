e948635be57c64f7728e49e8c0f84cc3dfba6eba
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-05-17 20:39:33 +0200

    Add evauluation method (only for ex2)

--- Alexander-Knueppel-Task2/src/application/features/classifier/AbstractClassifierFeature.java
+++ Alexander-Knueppel-Task2/src/application/features/classifier/AbstractClassifierFeature.java
@@ -1,13 +1,23 @@
 package application.features.classifier;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import application.features.Feature;
 import application.features.Feature.FeatureType;
 import framework.DataSet;
+import framework.classifier.DataPoint;
 
 public abstract class AbstractClassifierFeature implements Feature {
 
 	public AbstractClassifierFeature() {}
-
+	
+	public DataSet dataSet;
+//	public abstract void train(DataSet ds);
+//	public abstract void classify(DataPoint dp);
+	
+	public abstract String evaluate(DataSet ds); //<!!! only for ex2,... not enough time...
+	
 	public FeatureType getFeatureType() {
 		// TODO Auto-generated method stub
 		return Feature.FeatureType.FT_CLASSIFIER;
