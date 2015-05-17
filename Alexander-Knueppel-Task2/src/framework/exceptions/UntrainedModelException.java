477baba8e73bc54e150216a39e1156ebf079e74f
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-05-17 20:42:39 +0200

    Add exception

---
+++ Alexander-Knueppel-Task2/src/framework/exceptions/UntrainedModelException.java
@@ -0,0 +1,15 @@
+package framework.exceptions;
+
+public class UntrainedModelException extends Exception {
+
+	public UntrainedModelException(String string) {
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+}
