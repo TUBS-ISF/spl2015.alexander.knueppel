8d2ac27bff74460b265da7a748193f2ba8bd7578
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-05-17 14:27:16 +0200

    Add flie loading feature for arff files

---
+++ Alexander-Knueppel-Task2/src/application/features/fileloader/ArffFileLoaderFeature.java
@@ -0,0 +1,30 @@
+package application.features.fileloader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import framework.DataSet;
+import framework.fileloader.ArffFileLoader;
+
+public class ArffFileLoaderFeature extends AbstractFileLoaderFeature {
+
+	public ArffFileLoaderFeature() {}
+
+	@Override
+	public DataSet loadFile(String[] args) throws FileNotFoundException, IOException {
+		//we only need arg0
+		String filename = args[0];
+		return new ArffFileLoader().loadFile(new File(filename));
+	}
+
+	public String getIdentifier() {
+		// TODO Auto-generated method stub
+		return "arff-fileloader-feature";
+	}
+
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return "Implemented feature for loading *.arff-datafiles";
+	}
+}
