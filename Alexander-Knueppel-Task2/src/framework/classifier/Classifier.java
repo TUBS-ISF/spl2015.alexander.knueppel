a8e90945b7ac664039a98f840390bf8185227509
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-05-17 18:33:54 +0200

    Add training method

--- Alexander-Knueppel-Task2/src/framework/classifier/Classifier.java
+++ Alexander-Knueppel-Task2/src/framework/classifier/Classifier.java
@@ -1,5 +1,8 @@
 package framework.classifier;
 
+import framework.DataSet;
+
 public interface Classifier extends Cloneable {
 	public CategoricalResults classify(DataPoint data);
+	public void train(DataSet dataSet);
 }
