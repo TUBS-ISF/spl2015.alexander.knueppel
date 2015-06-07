3e94640b4f8a1a26cd6e94fb4837b03e06a90afb
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-06-07 09:27:10 +0200

    Add IB1 classifier feature

---
+++ Alexander-Knueppel-Task3/src/application/features/classifier/IB1ClassifierFeature.java
@@ -0,0 +1,36 @@
+package application.features.classifier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import framework.CategoricalData;
+import framework.DataSet;
+import framework.SimpleDataSet;
+import framework.classifier.CategoricalResults;
+import framework.classifier.Classifier;
+import framework.classifier.DataPoint;
+import framework.classifier.ZeroRClassifier;
+import framework.classifier.eval.Accuracy;
+import framework.classifier.eval.Kappa;
+import framework.classifier.eval.Score;
+import framework.classifier.knn.IB1;
+import framework.exceptions.UntrainedModelException;
+
+public class IB1ClassifierFeature extends AbstractClassifierFeature {
+
+	public String getIdentifier() {
+		// TODO Auto-generated method stub
+		return "ib1-classifier-feature";
+	}
+
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return "Implements instance based classification";
+	}
+
+	@Override
+	public Classifier getClassifier(DataSet ds, int catIndex) {
+		return new IB1(ds.getCategories()[catIndex],catIndex);
+	}
+}
