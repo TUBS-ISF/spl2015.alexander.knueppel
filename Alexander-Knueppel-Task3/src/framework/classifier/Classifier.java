f588daa0c8af1182188c9213898360e418ea8220
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-06-07 09:17:13 +0200

    update classifier interface

--- Alexander-Knueppel-Task3/src/framework/classifier/Classifier.java
+++ Alexander-Knueppel-Task3/src/framework/classifier/Classifier.java
@@ -6,4 +6,7 @@ import framework.exceptions.UntrainedModelException;
 public interface Classifier extends Cloneable {
 	public CategoricalResults classify(DataPoint data) throws UntrainedModelException;
 	public void train(DataSet dataSet);
+	public void clear();
+	public String getName();
+	public String getDescription();
 }
